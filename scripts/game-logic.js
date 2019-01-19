73fc31bd063ca11adc15c9db5065a712c01f4785
Jo-R <jo.rigg8@gmail.com>
2019-01-19 09:05:16 +0000

    initial hello world commit!

---
+++ scripts/game-logic.js
@@ -0,0 +1,8 @@
+class GameLogic {
+
+    logger() {
+        console.log("Hi");
+    }
+}
+
+export {GameLogic};
---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+import {GameLogic} from './game-logic.js';
+
+const game = new GameLogic();
+
+game.logger();
