97031b8becb4f2349110cc756a5af1b6930518b5
Jo-R <jo.rigg8@gmail.com>
2019-02-05 10:20:37 +0000

    add todo list and soem styling

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,7 @@
 import {GameLogic} from './game-logic.js';
 import { UiLogic } from './ui-logic.js';
 
-const testGrid = [
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 1, 1, 1, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0]
-]
 
-
-
-const createUI = new UiLogic(document.getElementById('grid'), 6);
+const createUI = new UiLogic(document.getElementById('grid'), 30);
 
 
